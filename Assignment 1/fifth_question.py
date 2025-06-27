51a8172aebf18fa7f4d2b6956f2e327f8b785bc0
vjtiarnav <114136029+vjtiarnav@users.noreply.github.com>
2025-06-27 11:33:50 +0530

    Update fifth_question.py

--- Assignment 1/fifth_question.py
+++ Assignment 1/fifth_question.py
@@ -4,7 +4,7 @@ principal = int(input("Enter the principal amount: "))
 rateOfInterest = float(input("Enter the rate of interest: "))
 years = int(input("Enter the number of years: "))
 
-compoundInterest = principal*(1+(0.01*rateOfInterest))**years
+amount = principal*(1+(0.01*rateOfInterest))**years
 
-print(f"The compound interest is {compoundInterest}")
-print(f"The total amount is {compoundInterest+principal}")
+print(f"The compound interest is {amount-principal}")
+print(f"The total amount is {amount}")
