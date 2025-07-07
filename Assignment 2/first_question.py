a5ab9f251ffabc89525ab963be93f20054fb3912
Arnav Harshad Chavan <114136029+vjtiarnav@users.noreply.github.com>
2025-07-07 21:12:16 +0530

    Delete Assignment 2 directory

--- Assignment 2/first_question.py
+++
@@ -1,9 +0,0 @@
-# Convert the time entered in hh, min and sec into seconds
-
-hours = int(input("Enter time in hours: "))
-mins = int(input("Enter time in minutes: "))
-sec = int(input("Enter time in seconds: "))
-
-timeInSeconds = (hours*60*60) + (mins*60) + sec
-
-print(f"{hours} hour {mins} minutes {sec} seconds in total is {timeInSeconds} seconds")
--- Assignment 2/second_question.py
+++
@@ -1,8 +0,0 @@
-# Convert temp from Celsius into Fahrenheit 
-
-celsius = float(input("Enter temperature in Celsius: "))
-
-fahrenheit = (9/5)*celsius + 32
-fahrenheit = round(fahrenheit,2)
-
-print(f"{celsius} °C is {fahrenheit} °F")
